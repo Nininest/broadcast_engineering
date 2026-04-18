d334e198da63bb074e5029f2d77da7f7c0fb6bd2
palishamaharjan <rmhrzn1@gmail.com>
2026-04-18 11:38:22 +0545

    Added URL paths for reports home, PDF download and Excel download

---
+++ reports/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.reports_home, name='reports_home'),
+    path('pdf/', views.generate_pdf_report, name='report_pdf'),
+    path('excel/', views.generate_excel_report, name='report_excel'),
+]
