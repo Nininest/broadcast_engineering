0ab359d2179055ec34033aae519762e0221b2a83
palishamaharjan <162278059+palishamaharjan@users.noreply.github.com>
2026-05-10 07:24:31 +0545

    Update reports_home URL path to include 'dashboard/'

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.reports_dashboard, name='reports_home'),
+    path('dashboard/', views.reports_dashboard, name='reports_home'),
     path('pdf/', views.generate_pdf_report, name='report_pdf'),
     path('excel/', views.generate_excel_report, name='report_excel'),
 ]
